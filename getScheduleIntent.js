b302171779c7a35fa385d90ee4fd6b0d3ae679a4
Peasplayer <peasplayer@peasplayer.tk>
2022-11-07 21:24:48 +0100

    Debug-Logs entfernt

--- getScheduleIntent.js
+++ getScheduleIntent.js
@@ -11,18 +11,13 @@ const GetScheduleIntent = {
         let prompt = "Es ist ein Fehler aufgetreten";
         const intent = handlerInput.requestEnvelope.request.intent;
 
-        //await db.set(Utils.getSlotValue(intent.slots["day"]).id + "." + intent.slots["lesson"], Utils.getSlotValue(intent.slots["subject"]).id)
-
         prompt = "Am " + Utils.getSlotValue(intent.slots["day"]).name + "hast du";
 
         for (let i = 1; i <= 8; i++) {
             let subject = await db.get(Utils.getSlotValue(intent.slots["day"]).id + "." + i)
-            console.log("2: " + i)
             if (subject !== undefined) {
-                console.log("3")
                 prompt += " in der " + Utils.getLessonString(i)
                     + " Stunde " + Utils.subjects[subject] + ","
-                console.log("4: " + prompt.toString())
             }
         }
 
