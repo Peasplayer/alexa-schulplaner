f6f123ed742f4e2200d9d58fcd05c6f3658a44f9
Peasplayer <63916422+Peasplayer@users.noreply.github.com>
2022-11-20 16:24:55 +0000

    Fixed tomorrow and today

--- getScheduleIntent.js
+++ getScheduleIntent.js
@@ -9,38 +9,30 @@ const GetScheduleIntent = {
     },
     async handle(handlerInput) {
         const intent = handlerInput.requestEnvelope.request.intent;
-        console.log("0a")
         let prompt = "Es ist ein Fehler aufgetreten";
-        console.log("0b" + intent.slots["day"])
         let day = Utils.getSlotValue(intent.slots["day"]).id;
-        console.log("0c")
-
-        console.log("1")
 
         if (Utils.getSlotValue(intent.slots["day"]).id === "today") {
             const date = new Date(Date.now());
-            if (date.getDay() >= 5)
+            if (date.getDay() >= 5 || date.getDay() === 0)
                 return handlerInput.responseBuilder.speak("Heute hast du keine Schule").getResponse();
 
-            day = Utils.days[date.getDay()];
+            day = Utils.days[date.getDay() - 1];
         }
-
-        console.log("2")
 
         if (Utils.getSlotValue(intent.slots["day"]).id === "tomorrow") {
-            const date = new Date(Date.now()) + 1;
-            if (date.getDay() >= 5)
+          const today = new Date()
+          const tomorrow = new Date(today)
+          tomorrow.setDate(tomorrow.getDate() + 1)
+            if (tomorrow.getDay() >= 5 || tomorrow.getDay() === 0)
                 return handlerInput.responseBuilder.speak("Morgen hast du keine Schule").getResponse();
 
-            day = Utils.days[date.getDay()];
+            day = Utils.days[tomorrow.getDay() - 1];
         }
 
-        console.log("3: " + day)
-
-        prompt = Utils.getSlotValue(intent.slots["day"]).name + "hast du";
+        prompt = Utils.getSlotValue(intent.slots["day"]).name + " hast du";
 
         for (let i = 1; i <= 8; i++) {
-            console.log("4")
             let subject = await db.get(day + "." + i)
             if (subject !== undefined) {
                 prompt += " in der " + Utils.getLessonString(i)
